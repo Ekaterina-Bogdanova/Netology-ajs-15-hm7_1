961cd4a69bc92681c3616a4ffd5cf496b4c82375
Ekaterina-Bogdanova <sivtera09@gmail.com>
2021-09-17 00:25:29 +0300

    test corrected

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
 export default class Validator {
-  constructor (userName) {
-    this.userName = userName;  
+  constructor(userName) {
+    this.userName = userName;
   }
 
   validateUsername() {
     return (!/[^-\w]/.test(this.userName) && /^[^-_\d].+[^-_\d]$/.test(this.userName) && !/\d{4,}/.test(this.userName));
   }
 }
-
-const person = new Validator('Ralf0099p');
-person.validateUsername();
-
-
